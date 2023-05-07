153d6b77d1d63a663da63f65cccf9509e55d2984
Maheshwor Chaudhary <111302302+raazz1258@users.noreply.github.com>
2023-05-07 14:57:36 +0545

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -11,8 +11,8 @@ function send_email($code, $send_to)
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'raazzchaudhary9@gmail.com';
-    $mail->Password = 'nnqpilugawyfheyw';
+    $mail->Username = 'your email';
+    $mail->Password = 'your app password';
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
 
